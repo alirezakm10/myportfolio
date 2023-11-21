5a3636f092fd210d938dfd140586bca25fbdef79
alirezakm10 <alireza.version@gmail.com>
2023-11-22 00:59:11 +0330

    vite config

--- runDocker.sh
+++ runDocker.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-#git remote set-url origin git@github.com-parax-front:frontend/CMS_Parax_FrontEnd.git
-#git pull origin main
-sudo docker compose -f docker-compose.production.yml up -d --build --force-recreate
+docker build -t alirezakm-portfoli  .
+
+docker run -p 3000:3000 alirezakm-portfoli 
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
+  server: {
+    host: true,
+    port: 3000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
+  }
 })
