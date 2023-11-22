8b61774a4635e2823a45faa436bab2c4b707b2c9
alireza karbalayi morad <alireza.version@gmail.com>
2023-11-22 12:05:12 +0330

    esedgdsg

--- runDocker.sh
+++ runDocker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
- docker build -t vite-3d-portfolio:latest .
+sudo docker compose -f docker-compose.yml up -d --build --force-recreate
+#  docker build -t vite-3d-portfolio:latest .
 
-docker run -p 8080:8080 vite-3d-portfolio:latest
+# docker run -p 8080:8080 vite-3d-portfolio:latest
 
 
-docker run -p 3000:3000 vite-3d-portfolio:latest
+# docker run -p 3000:3000 vite-3d-portfolio:latest
