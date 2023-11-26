c0f251100e2067ecd371713846ba799735438afb
alirezakm10 <alireza.version@gmail.com>
2023-11-27 01:20:50 +0330

    dfghsdfgh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 = "prod" || $1 = "dev" ]] && [[ $2 = "down" || $2 = "up" ]]; then
     cd ..
-    fileEnv="docker compose.${1}.yaml"
+    fileEnv="docker-compose.${1}.yaml"
     downOrUp=$2
     echo "Running docker compose -f docker-compose.yaml -f $fileEnv $downOrUp"
     dockercompose -f docker-compose.yaml -f $fileEnv $downOrUp
