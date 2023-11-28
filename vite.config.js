c5a8b8f9f4c304fdeb101cfb95d7cacc31528db7
alirezakm10 <alireza.version@gmail.com>
2023-11-28 22:32:19 +0330

    sdgsdfgh

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  preview: {
+  server: {
     host:true,
     strictPort:true,
     port: 3010
